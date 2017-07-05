d0fee28b9615fb676081d1dd5cf7f1ac6f896fc8
alyssadelaine <alyssadelaine@gmail.com>
2017-07-05 16:14:36 -0400

    Simulate more closely calling exes in groups of 4

--- POC.py
+++ POC.py
@@ -18,34 +18,34 @@ import time
 # Executable prep done in a separate file
 from executable_prep import prep_for_run
 
-tot_plan = 40
-my_list = [-1] * tot_plan
+tot_plan = 10
+num_slices = 4
+my_list = [-1] * tot_plan * num_slices
 
 first_lock = threading.Event()
 
-def call_exe(plan_num):
+def call_exe(plan_num, db_ptr):
 	global my_list, first_lock
 
 	cmdList = prep_for_run(plan_num)
-	print(str(cmdList))
+	exe_num = (plan_num + 1) * num_slices - (db_ptr + 1)
 	# call the executable
 	try:
-		print(str(cmdList))
 		if plan_num == 0:
-			print("I'm about to sleep! \n")
+			# print("I'm about to sleep! \n")
 			# time.sleep(12)
-			print ("Now call subprocess \n")
-			my_list[plan_num] = subprocess.call(cmdList) #, stdout=subprocess.PIPE) # subprocess.check_output(cmdList)
-			print("Wake up main threads! \n")
-			first_lock.set()
+			# print ("Now call subprocess \n")
+			my_list[exe_num] = subprocess.call(cmdList, stdout=subprocess.PIPE) # subprocess.check_output(cmdList)
+			# print("Wake up main threads! \n")
+			# first_lock.set()
 		else:
 			# time.sleep(12)
-			print ("Plan number {} about to execute".format(plan_num))
-			my_list[plan_num] = subprocess.call(cmdList, stdout=subprocess.PIPE) # subprocess.check_output(cmdList)
+			# print ("Plan number {} about to execute".format(exe_num))
+			my_list[exe_num] = subprocess.call(cmdList, stdout=subprocess.PIPE) # subprocess.check_output(cmdList)
 	except OSError as e:
 		errno, strerror = e.args
 		print("OS error({0}): {1}".format(errno,strerror))
-		print("When trying to acces: {}".format(cmdList))
+		print("When trying to access: {}".format(cmdList))
 	except TypeError as e:
 		print(str(sys.exc_info()[0]) + ': ' + str(e))
 	except IndexError as e:
@@ -53,27 +53,36 @@ def call_exe(plan_num):
 	except:
 		print(str(sys.exc_info()[0]))
 
+def sum_groups(plan):
+	json_list = []
+	for csvR in range(0, num_slices):
+		json_list.append(get_results(csvR))
+
+
 def manager():
+	global help_list
 	start = time.time()
-	print("My list is: {}".format(my_list))
 	thread_list = []
 
 	for plan in range(0,tot_plan):
-		t = threading.Thread(target = call_exe, name = str(plan), args = (plan,))
-		thread_list.append(t)
-		t.start()
+		for db_index in range(0, num_slices):
+			t = threading.Thread(target = call_exe, name = str(plan), args = (plan,db_index,))
+			thread_list.append(t)
+			t.start()
 
-	print("Main thread about to wait")
-	first_lock.wait()
-	print("*********")
+	# print("Main thread about to wait")
+	# first_lock.wait()
+	# print("*********")
 
-	print("MAIN THREAD IS BACK")
+	# print("MAIN THREAD IS BACK")
 	# Here is where the second portion of the first executable will be called.
 
-	"""
+	# """
 	for t_ptr in thread_list:
 		t_ptr.join()
 	"""
+
+	# This is functionally the same as above
 
 	while sum(my_list) != 0:
 		# print("******** ")
