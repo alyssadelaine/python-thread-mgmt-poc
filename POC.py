340e726b6fe4cd03b16bed4ba7df49aff17a14c9
alyssadelaine <alyssadelaine@gmail.com>
2017-07-05 14:56:34 -0400

    minor fix

--- POC.py
+++ POC.py
@@ -50,7 +50,6 @@ plans_queued = []
 all_results = {}
 
 # hold the output files, which I'll build in another file
-
 output_files = [None]*tot_plan*num_slices
 # use a lock to avoid race conditions relative to plans_complete
 event_lock = threading.Lock()
